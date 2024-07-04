9d91a5456f2e94ab6ecdee4bfd05581d32c5c508
Sergey Inozemtsev <inozemtsevs@yandex.ru>
2024-07-04 17:52:30 +0300

    Alembic is added

--- application/database/db_connection.py
+++ application/database/db_connection.py
@@ -3,8 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from configs import (application, DB_NAME, DB_PASSWORD, DB_USERNAME,
                      SERVER_NAME)
 
-application.config['SQLALCHEMY_DATABASE_URI'] = (
-    f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{SERVER_NAME}/{DB_NAME}"
-)
+DB_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{SERVER_NAME}/{DB_NAME}"
+application.config['SQLALCHEMY_DATABASE_URI'] = (DB_URL)
 
 db = SQLAlchemy(application)
