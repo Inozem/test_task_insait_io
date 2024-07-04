df91b4454870bfde0ffc1a7c22e717110b63f854
Sergey Inozemtsev <inozemtsevs@yandex.ru>
2024-07-04 11:48:40 +0300

    The db model is created

--- application.py
+++ application.py
@@ -1,7 +1,6 @@
 from configs import application
-# from database.db_connection import db
+from database.db_connection import db
 from routes import ask_route
-
 
 if __name__ == "__main__":
     application.run(debug=True, host="0.0.0.0", port=5000)
--- configs.py
+++ configs.py
@@ -8,4 +8,9 @@ application = Flask(__name__)
 load_dotenv()
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
+SERVER_NAME = os.getenv('SERVER_NAME')
+DB_USERNAME = os.getenv('DB_USERNAME')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_NAME = os.getenv('DB_NAME')
+
 
